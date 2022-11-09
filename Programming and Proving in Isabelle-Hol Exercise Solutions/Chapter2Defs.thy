086ca228b8d002207875bc23f65c1159df55e14a
Murat Koptur <mkoptur3@gmail.com>
2022-11-09 16:10:53 +0300

    ch2 continues

--- Programming and Proving in Isabelle-Hol Exercise Solutions/Chapter2Defs.thy
+++ Programming and Proving in Isabelle-Hol Exercise Solutions/Chapter2Defs.thy
@@ -6,10 +6,15 @@ fun add :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
 "add 0 n = n" |
 "add (Suc m) n = Suc(add m n)"
 
-lemma add_02 [simp]: "add m 0 = m"
+lemma add_02[simp] : "add m 0 = m"
   apply(induction m)
   apply(auto)
   done
 
+datatype 'a tree = Tip | Node "'a tree" 'a "'a tree"
+
+fun mirror:: "'a tree \<Rightarrow> 'a tree" where
+"mirror Tip = Tip" |
+"mirror (Node l a r) = Node (mirror r) a (mirror l)"
 
 end
--- Programming and Proving in Isabelle-Hol Exercise Solutions/Chapter2Sols.thy
+++ Programming and Proving in Isabelle-Hol Exercise Solutions/Chapter2Sols.thy
@@ -72,4 +72,49 @@ lemma "sum_upto n = (n * (n+1)) div 2"
    apply(auto)
   done
 
+(*  Exercise 2.6  *)
+fun contents:: "'a tree \<Rightarrow> 'a list" where
+"contents Tip = []" |
+"contents (Node l a r) = (Cons a (contents l))@(contents r)"
+
+fun sum_tree:: "nat tree \<Rightarrow> nat" where
+"sum_tree Tip = 0" |
+"sum_tree (Node l a r) = a + sum_tree l + sum_tree r"
+
+lemma "sum_tree t = sum_list(contents t)"
+  apply(induction t)
+   apply(auto)
+  done
+
+(*  Exercise 2.7  *)
+fun pre_order:: "'a tree \<Rightarrow> 'a list" where
+"pre_order Tip = []" |
+"pre_order (Node l a r) = [a]@(pre_order l)@(pre_order r)"
+
+fun post_order:: "'a tree \<Rightarrow> 'a list" where
+"post_order Tip = []" |
+"post_order (Node l a r) = (post_order l)@(post_order r)@[a]"
+
+lemma "pre_order (mirror t) = rev (post_order t)"
+  apply(induction t)
+  apply(auto)
+  done
+
+(*  Exercise 2.8  *)
+fun intersperse :: "'a \<Rightarrow> 'a list \<Rightarrow> 'a list" where
+"intersperse elem [] = [elem]" |
+"intersperse elem (x#xs) = [x] @ [elem] @ (intersperse elem xs)"
+
+lemma "map f (intersperse a xs) = intersperse (f a) (map f xs)"
+  apply(induction xs)
+   apply(auto)
+  done
+
+(*  Exercise 2.9  *)
+fun itadd:: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+"itadd 0 n = n" |
+"itadd (Suc m) n = itadd m (Suc n)"
+
+(* proof is incomplete *)
+
 end
