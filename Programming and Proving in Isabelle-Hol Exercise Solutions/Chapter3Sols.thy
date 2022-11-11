73fd3618fb80d4635000559a1cedce9a50dd98e8
Murat Koptur <mkoptur3@gmail.com>
2022-11-11 21:47:58 +0300

    ex 3.5 by sledgehammer

--- Programming and Proving in Isabelle-Hol Exercise Solutions/Chapter3Sols.thy
+++ Programming and Proving in Isabelle-Hol Exercise Solutions/Chapter3Sols.thy
@@ -19,5 +19,43 @@ lemma "palindrome xs \<Longrightarrow> rev xs = xs"
   apply(induction xs rule: palindrome.induct)
     apply(simp_all)
   done
+
+(*  Exercise 3.3  *)
+(* not complete *)
+
+(*  Exercise 3.4  *)
+(* not complete *)
+
+(*  Exercise 3.5  *)
+(* Adapted from: https://isabelle.in.tum.de/exercises/logic/parentheses/sol.pdf *)
+datatype alpha = a | b
+
+inductive S:: "alpha list \<Rightarrow> bool" where
+S1: "S []" |
+S2: "S [v] \<Longrightarrow> S (a # [v] @ [b])" |
+S3: "S [v] \<Longrightarrow> S [w] \<Longrightarrow> S([v] @ [w])"
+
+declare S1 [iff] S2 [intro!, simp]
+
+inductive T:: "alpha list \<Rightarrow> bool" where
+T1: "T []" |
+T2: "T [v] \<Longrightarrow> T[w] \<Longrightarrow> T([v] @ a # [w] @ [b])"
+
+declare T1 [iff]
+
+lemma T2S:"T w \<Longrightarrow> S w"
+  apply(erule T.induct)
+   apply(simp)
+  (* found by sledgehammer *)
+  using S.cases by auto
+
+lemma S2T:"S w \<Longrightarrow> T w"
+  apply(erule S.induct)
+    apply(simp)
+  (* found by sledgehammer *)
+  using S.cases by auto
+
+lemma "S w == T w"
+  by (smt (verit) S2T T2S)
   
 end
